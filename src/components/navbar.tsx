74f5a60dec0f2ec561a160edb0a7d48a0723fb94
zachuri <punsalang.zachary@gmail.com>
2023-02-14 09:58:03 -0800

    navbar: fixed-> all in one place

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const links = [
   { href: "/projects", name: "Projects" },
-  { href: "/about", name: "About Us" },
-  { href: "/contact", name: "Contact Us" },
+  { href: "/about", name: "About" },
+  { href: "/contact", name: "Contact" },
   { href: "/news", name: "News" },
   { href: "/clients", name: "Clients" },
 ];
@@ -12,24 +12,31 @@ const links = [
 const Navbar = () => {
   return (
     <div className="fixed top-0 z-[100] mt-10 w-full">
-      <div className="flex flex-row justify-between p-10">
-        <div className=" bg-[#282928] p-5 uppercase">
-          <h1 style={{ letterSpacing: 5 }} className="text-4xl text-[#D8D7D4]">
-            ArcelDesign, Inc.
-          </h1>
-          <p style={{ letterSpacing: 5 }} className="text-xl text-[#fff]">
-            Interior Design + Build
-          </p>
-        </div>
-
-        <div className="flex flex-wrap items-center">
-          {links.map((link) => (
-            <div key={link.name} className="p-4">
-              <Link href={link.href}>
-                <button>{link.name}</button>
-              </Link>
+      <div className="flex flex-row-reverse p-10">
+        <div className="bg-[#282928] drop-shadow-lg backdrop-blur-lg">
+          <div className="p-5 uppercase">
+            <h1
+              style={{ letterSpacing: 5 }}
+              className="text-4xl text-[#838383]"
+            >
+              ArcelDesign, Inc.
+            </h1>
+            <p
+              style={{ letterSpacing: 5 }}
+              className="text-center text-xl text-[#fff]"
+            >
+              Interior Design + Build
+            </p>
+            <div className="flex items-center justify-center text-[#fff]">
+              {links.map((link) => (
+                <div key={link.name} className="text-md -mb-3 mt-2 p-3">
+                  <Link href={link.href}>
+                    <button>{link.name}</button>
+                  </Link>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
