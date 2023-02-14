c7bc543fe2f0a0d9b5d49b00a2bfe70e53979ef7
zachuri <punsalang.zachary@gmail.com>
2023-02-14 10:05:27 -0800

    navbar: added hover for navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,22 +14,22 @@ const Navbar = () => {
     <div className="fixed top-0 z-[100] mt-10 w-full">
       <div className="flex flex-row-reverse p-10">
         <div className="bg-[#282928] drop-shadow-lg backdrop-blur-lg">
-          <div className="p-5 uppercase">
-            <h1
-              style={{ letterSpacing: 5 }}
-              className="text-4xl text-[#838383]"
-            >
+          <div className="-pb-5 p-5 uppercase">
+            <h1 style={{ letterSpacing: 5 }} className="text-4xl text-[#fff]">
               ArcelDesign, Inc.
             </h1>
             <p
               style={{ letterSpacing: 5 }}
-              className="text-center text-xl text-[#fff]"
+              className="mb-5 text-center text-xl text-[#838383]"
             >
               Interior Design + Build
             </p>
-            <div className="flex items-center justify-center text-[#fff]">
+            <div className="flex items-center justify-center space-x-3 text-[#fff]">
               {links.map((link) => (
-                <div key={link.name} className="text-md -mb-3 mt-2 p-3">
+                <div
+                  key={link.name}
+                  className="text-md p-1 pb-1 hover:text-[#838383]"
+                >
                   <Link href={link.href}>
                     <button>{link.name}</button>
                   </Link>
