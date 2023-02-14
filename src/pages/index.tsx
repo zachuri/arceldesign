8240a516a12ff7044fefe38f41e961ce98c31a48
zachuri <punsalang.zachary@gmail.com>
2023-02-14 12:15:53 -0800

    index: added height screen from safari and chrome in its own div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
-      <Slider />
+      <div className="supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]">
+        <Navbar />
+        <Slider />
+      </div>
     </>
   );
 };
