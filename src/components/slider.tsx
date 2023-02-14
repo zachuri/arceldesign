6ab1f7235ab6b4399a9437fef9685ed3676cdaf9
zachuri <punsalang.zachary@gmail.com>
2023-02-14 08:32:25 -0800

    slider: added link for each slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -10,13 +10,39 @@ import "swiper/css/effect-fade";
 // import required modules
 import SwiperCore, { Autoplay, Navigation, EffectFade } from "swiper";
 import Image from "next/image";
+import Link from "next/link";
 
 const images = [
-  { src: "/assets/home/home1.jpg", alt: "home 1", type: "Hospitatily" },
-  { src: "/assets/home/home2.jpg", alt: "home 2", type: "Dental" },
-  { src: "/assets/home/home3.jpg", alt: "home 3", type: "Medical" },
-  { src: "/assets/home/home3.jpg", alt: "home 3", type: "Residential" },
-  { src: "/assets/home/home3.jpg", alt: "home 3", type: "Corprate" },
+  {
+    src: "/assets/home/home1.jpg",
+    alt: "home 1",
+    href: "/hospitality",
+    type: "Hospitality",
+  },
+  {
+    src: "/assets/home/home2.jpg",
+    alt: "home 2",
+    href: "/dental",
+    type: "Dental",
+  },
+  {
+    src: "/assets/home/home3.jpg",
+    alt: "home 3",
+    href: "/medical",
+    type: "Medical",
+  },
+  {
+    src: "/assets/home/home3.jpg",
+    alt: "home 3",
+    href: "/residential",
+    type: "Residential",
+  },
+  {
+    src: "/assets/home/home3.jpg",
+    alt: "home 3",
+    href: "/corporate",
+    type: "Corporate",
+  },
 ];
 
 const Slider = () => (
@@ -55,8 +81,10 @@ const Slider = () => (
                 className="bg-[#282928] p-2 font-light "
               >
                 <h2 className="px-2 text-2xl uppercase text-[#838383]">
-                  Projects /{" "}
-                  <span className="text-[#D8D7D4]">{image.type}</span>
+                  <Link href={image.href}>
+                    Projects /{" "}
+                    <span className="text-[#D8D7D4]">{image.type}</span>
+                  </Link>
                 </h2>
               </div>
             </div>
