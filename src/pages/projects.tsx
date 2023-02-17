27429f8d3a4278273895e7094637b2423615c661
zachuri <punsalang.zachary@gmail.com>
2023-02-17 04:29:34 -0800

    project/s: added key carouselitem

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,36 +5,33 @@ const dentals = [
   { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
   { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
   { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
+];
+
+const corparte = [
   { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
   { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
   { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
 ];
 
-const corprate = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-];
-
-const medical = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-];
-
 const hospitality = [
   { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
 ];
 
 const residential = [
   { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
 ];
 
 const Projects = () => {
   return (
     <div className="bg-[#fbfbfb]">
       <Project projectType="Dental" projects={dentals} />
-      <Project projectType="Corprate" projects={corprate} />
+      <Project projectType="Corprate" projects={corparte} />
       <Project projectType="hospitality" projects={hospitality} />
       <Project projectType="residential" projects={residential} />
     </div>
