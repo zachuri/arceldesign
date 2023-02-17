c21ee8f1b0b22e7965821dca4ebeb88a226191c4
zachuri <punsalang.zachary@gmail.com>
2023-02-17 06:41:20 -0800

    project: added information for each project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -4,25 +4,34 @@ import Image from "next/image";
 
 interface Props {
   projectType: String;
-  projects: { src: string; alt: string }[];
+  projects: { src: string; alt: string; area: string; address: string }[];
 }
 
 const Project: React.FC<Props> = ({ projectType, projects }) => {
   return (
     <>
-      <h1 className="text-2xl">{projectType}</h1>
+      <h1
+        style={{ letterSpacing: 10 }}
+        className="text-2xl uppercase text-[#222222]"
+      >
+        {projectType}
+      </h1>
       <Carousel className=" py-10 text-white">
         {projects.map((project, index) => {
           return (
             // NOTE: carousel needs index -> either manually or index with map
             <CarouselItem index={index} key={index}>
-              <Image
-                src={project.src}
-                alt={project.alt}
-                width={500}
-                height={500}
-                loading="lazy"
-              />
+              <>
+                <Image
+                  src={project.src}
+                  alt={project.alt}
+                  width={500}
+                  height={500}
+                  loading="lazy"
+                />
+                <h1 className="text-[#222222]">{project.area}</h1>
+                <p className="text-[#222222]">{project.address}</p>
+              </>
             </CarouselItem>
           );
         })}
