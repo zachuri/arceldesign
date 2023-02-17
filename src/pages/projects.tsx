f602cb4389aa87480d38819045565bb677999990
zachuri <punsalang.zachary@gmail.com>
2023-02-16 18:43:07 -0800

    projects: created working reusable project component with carousel

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,149 +3,48 @@ import Carousel from "../components/carousel";
 import CarouselItem from "../components/carousel-item";
 import Image from "next/image";
 
-const Projects = () => {
+const dentals = [
+  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
+  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
+];
+
+interface Props {
+  projectType: String;
+  projects: { src: string; alt: string }[];
+}
+
+const Project: React.FC<Props> = ({ projectType, projects }) => {
   return (
     <>
+      <h1 className="text-2xl">{projectType}</h1>
       <Carousel className=" text-white lg:py-10">
-        <CarouselItem index={0}>
-          <Image
-            src="/assets/home/home1.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={1}>
-          <Image
-            src="/assets/home/home2.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={2}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={3}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-      </Carousel>
-
-      <Carousel className=" text-white lg:py-10">
-        <CarouselItem index={0}>
-          <Image
-            src="/assets/home/home1.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={1}>
-          <Image
-            src="/assets/home/home2.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={2}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={3}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-      </Carousel>
-
-      <Carousel className="text-white lg:py-10">
-        <CarouselItem index={0}>
-          <Image
-            src="/assets/home/home1.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={1}>
-          <Image
-            src="/assets/home/home2.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={2}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={3}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-      </Carousel>
-
-      <Carousel className="text-white lg:py-10">
-        <CarouselItem index={0}>
-          <Image
-            src="/assets/home/home1.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={1}>
-          <Image
-            src="/assets/home/home2.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={2}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
-        <CarouselItem index={3}>
-          <Image
-            src="/assets/home/home3.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-        </CarouselItem>
+        {projects.map((project, index) => {
+          return (
+            // NOTE: carousel needs index -> either manually or index with map
+            <CarouselItem index={index}>
+              <Image
+                src={project.src}
+                alt={project.alt}
+                width={500}
+                height={500}
+              />
+            </CarouselItem>
+          );
+        })}
       </Carousel>
     </>
+  );
+};
+
+const Projects = () => {
+  return (
+    <div className="bg-[#fbfbfb]">
+      <Project projectType="Dental" projects={dentals} />
+    </div>
   );
 };
 
