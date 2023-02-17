0a1ef8ffbc348486d5a7a01bf2d9ce4bfd343936
zachuri <punsalang.zachary@gmail.com>
2023-02-17 06:41:42 -0800

    projects: start to added data into json file

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,41 +1,13 @@
 import React from "react";
 import Project from "../components/project";
 import MainLayout from "../components/layout";
-
-const dentals = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-];
-
-const corparte = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-];
-
-const hospitality = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-];
-
-const residential = [
-  { src: "/assets/home/home1.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home2.jpg", alt: "Picture of the author" },
-  { src: "/assets/home/home3.jpg", alt: "Picture of the author" },
-];
+import dental from "../data/dental.json";
 
 const Projects = () => {
   return (
     <MainLayout>
       <div className="bg-[#fbfbfb]">
-        <Project projectType="Dental" projects={dentals} />
-        <Project projectType="Corprate" projects={corparte} />
-        <Project projectType="hospitality" projects={hospitality} />
-        <Project projectType="residential" projects={residential} />
+        <Project projectType="Dental" projects={dental} />
       </div>
     </MainLayout>
   );
