64dda247b38661c8c698efc4b9dbb78c637a966c
zachuri <punsalang.zachary@gmail.com>
2023-02-18 11:26:21 -0800

    projects: added medical and corporate

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Project from "../components/project";
 import MainLayout from "../components/layout";
 import dental from "../data/dental.json";
+import corporate from "../data/corporate.json";
+import medical from "../data/medical.json";
 
 const Projects = () => {
   return (
     <MainLayout>
       <div className="bg-[#fbfbfb]">
         <Project projectType="Dental" projects={dental} />
-        <Project projectType="Dental" projects={dental} />
-        <Project projectType="Dental" projects={dental} />
-        <Project projectType="Dental" projects={dental} />
+        <Project projectType="Corporate" projects={corporate} />
+        <Project projectType="Medical" projects={medical} />
       </div>
     </MainLayout>
   );
