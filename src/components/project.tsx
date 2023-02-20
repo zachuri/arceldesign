875c4d00558d864a2ea4c5eeba2b4013b31da3fb
zachuri <punsalang.zachary@gmail.com>
2023-02-19 20:30:59 -0800

    add: framer motion -> motion for project page

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,6 +1,7 @@
 import Carousel from "./carousel";
 import CarouselItem from "./carousel-item";
 import Image from "next/image";
+import LayoutMotion from "./motion";
 
 interface Props {
   projectType: String;
@@ -9,40 +10,42 @@ interface Props {
 
 const Project: React.FC<Props> = ({ projectType, projects }) => {
   return (
-    <div className="mx-5 ">
-      <h1
-        style={{ letterSpacing: 10 }}
-        className="py-5 uppercase text-[#222222] md:text-lg lg:text-2xl"
-      >
-        {projectType}
-      </h1>
-      <Carousel className=" text-white">
-        {projects.map((project, index) => {
-          return (
-            // NOTE: carousel needs index -> either manually or index with map
-            <CarouselItem index={index} key={index}>
-              <>
-                <img
-                  src={project.src}
-                  alt={project.alt}
-                  height={500}
-                  width={500}
-                />
-                {/* full padding breaks the slide */}
-                <div className="pl-5 pt-5 pb-5">
-                  <h1 className="text-md md:text-md uppercase tracking-widest text-[#222222] lg:text-xl">
-                    {project.area}
-                  </h1>
-                  <p className="text-sm text-[#222222] md:text-sm lg:text-lg">
-                    {project.address}
-                  </p>
-                </div>
-              </>
-            </CarouselItem>
-          );
-        })}
-      </Carousel>
-    </div>
+    <LayoutMotion>
+      <div className="mx-5 ">
+        <h1
+          style={{ letterSpacing: 10 }}
+          className="py-5 uppercase text-[#222222] md:text-lg lg:text-2xl"
+        >
+          {projectType}
+        </h1>
+        <Carousel className=" text-white">
+          {projects.map((project, index) => {
+            return (
+              // NOTE: carousel needs index -> either manually or index with map
+              <CarouselItem index={index} key={index}>
+                <>
+                  <img
+                    src={project.src}
+                    alt={project.alt}
+                    height={500}
+                    width={500}
+                  />
+                  {/* full padding breaks the slide */}
+                  <div className="pl-5 pt-5 pb-5">
+                    <h1 className="text-md md:text-md uppercase tracking-widest text-[#222222] lg:text-xl">
+                      {project.area}
+                    </h1>
+                    <p className="text-sm text-[#222222] md:text-sm lg:text-lg">
+                      {project.address}
+                    </p>
+                  </div>
+                </>
+              </CarouselItem>
+            );
+          })}
+        </Carousel>
+      </div>
+    </LayoutMotion>
   );
 };
 
