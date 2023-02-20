c75e5a9e3af84c8d7345b8ea593b7519908861c5
zachuri <punsalang.zachary@gmail.com>
2023-02-20 09:45:08 -0800

    bug/fix: prev solution wrong for carousel -> edit carousel item -> condition for navigation works

--- src/components/carousel-item.tsx
+++ src/components/carousel-item.tsx
@@ -1,26 +1,42 @@
 import React, { useContext, useCallback } from "react";
 import { CarouselContext } from "./carousel";
 import styles from "../styles/carousel.module.css";
+import { useRouter } from "next/router";
 
 interface Props {
   index: number;
+  navigate: string;
   children: JSX.Element;
 }
 
-const CarouselItem: React.FC<Props> = ({ children, index }) => {
+const CarouselItem: React.FC<Props> = ({ children, index, navigate }) => {
   const { embla: emblaApi, selectedIndex } = useContext(CarouselContext);
   const isActive = selectedIndex === index;
   const handleClick = useCallback(() => {
-    if (emblaApi === undefined) return 
+    if (emblaApi === undefined) return;
     emblaApi.scrollTo(index);
   }, [emblaApi, index]);
+
+  const nextRouter = useRouter();
 
   return (
     <div
       className={`${styles.slide} relative ${isActive ? "active" : ""}`}
       onClick={handleClick}
     >
-      {children}
+      {isActive ? (
+        <button
+          onClick={() => {
+            nextRouter.push({
+              pathname: navigate,
+            });
+          }}
+        >
+          {children}
+        </button>
+      ) : (
+        children
+      )}
     </div>
   );
 };
