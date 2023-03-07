9052f28373adb27871b7e50bd2fb9767f79d8b69
zachuri <punsalang.zachary@gmail.com>
2023-03-06 22:07:59 -0800

    layout: added about page layout and export all functions

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return <div className="mt-[59px] md:mt-[83px]">{children}</div>;
 };
 
-export default MainLayout;
+export const AboutLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return <div className="max-md:mt-[59px]">{children}</div>;
+};
