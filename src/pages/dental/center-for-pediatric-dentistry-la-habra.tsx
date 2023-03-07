44fd2078ab1a0ae300ce8e3c78a647699a95ef51
artifactz1 <punsalang.art@gmail.com>
2023-03-06 21:56:30 -0800

    starting to fix mobile version of page

--- src/pages/dental/center-for-pediatric-dentistry-la-habra.tsx
+++ src/pages/dental/center-for-pediatric-dentistry-la-habra.tsx
@@ -8,41 +8,46 @@ import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import { IoIosArrowDropright, IoIosArrowRoundForward } from "react-icons/io";
 
+import Image from "next/image";
+
 const COPLH = () => {
   return (
     <main>
-      <div className="mt-[250px] h-full w-full">
+      <div className="mx-5 mt-[250px] md:mx-40">
         {/* ===========================================================================*/}
         {/* Main Page */}
         {/* ===========================================================================*/}
-        <div className="ml-40 mr-40 grid grid-cols-2 text-white">
+        <div className=" flex flex-col text-white md:grid md:grid-cols-2">
           {/* ====  left ====*/}
-          <div>
+          <div className="relative">
             <div className="ml-2 mb-5 text-[10]">
               <div>Corporate Design</div>
               <div>2014</div>
             </div>
-            <div className="text-9xl">NOVA WORLD, INC</div>
-            <div className="mt-16 grid grid-cols-2">
-              <IoIosArrowDropright size="120"></IoIosArrowDropright>
-              <div className="flex flex-row-reverse">
-                <div className="flex flex-col-reverse">
-                  <div className="font-extralight">San Dimas, California</div>
-                  <div className="font-bold">Location</div>
-                </div>
+            <div className="mx-5 text-7xl  md:mx-0 md:text-9xl">
+              NOVA WORLD, INC
+            </div>
+            <div className="relative -mt-14 flex md:mt-16">
+              <IoIosArrowDropright
+                className="invisible md:visible "
+                size="120"
+              ></IoIosArrowDropright>
+              <div className="absolute right-0 bottom-0 flex flex-col">
+                <div className="font-bold">Location</div>
+                <div className="font-extralight">San Dimas, California</div>
               </div>
             </div>
           </div>
           {/* ==== right ====*/}
           <img
-            className="ml-20 object-fill"
+            className="mt-5 object-fill md:mt-0 md:ml-20"
             src={"/assets/dental/dental1/IMG_6566.jpg"}
             alt={"dental 1"}
           />
         </div>
 
         {/* ==== bottom ===*/}
-        <div className="mr-20 mt-10 ml-20 text-white">
+        <div className="mt-10 text-white ">
           <div className="flex flex-row space-x-10">
             <div className="flex flex-row">
               Gallery
@@ -56,12 +61,12 @@ const COPLH = () => {
 
           <div className="border-b-1 mt-5 mb-5 flex-grow border-t-2 border-white"></div>
 
-          <div className="flex-cols-2 mb-10 flex text-sm">
-            <div className="left-0 flex flex-col">
+          <div className="relative mb-10 flex text-sm">
+            <div className="absoute left-0 flex flex-col">
               <div className="font-bold">Corporate Collections</div>
               <div className="font-light">Collection 01</div>
             </div>
-            <div className="absolute right-0 mr-20 flex flex-col">
+            <div className="absolute right-0 flex flex-col">
               <div className="font-bold">Creative Design</div>
               <div className="font-light">Nova World, Inc</div>
             </div>
@@ -97,7 +102,9 @@ const COPLH = () => {
               <div className="font-light">ABOUT COLLECTIONS </div>
             </div>
             <div className="mt-[150px]">
-              <div className="text-[50px] font-bold">HARMONIOUS PALETTE</div>
+              <div className=" text-2xl font-bold md:text-[50px]">
+                HARMONIOUS PALETTE
+              </div>
               <div className="mt-10 text-xl font-medium">
                 Bringing natural soothing elements to set a harmonious tone. By
                 combining natural elements, and warm colors to the design, where
@@ -107,7 +114,9 @@ const COPLH = () => {
             </div>
           </div>
           <div className="mt-20">
-            <img
+            <Image
+              width={300}
+              height={300}
               className="h-[750px] w-full object-cover"
               src={"/assets/dental/dental1/IMG_6567.jpg"}
               alt={"dental 1"}
@@ -224,9 +233,9 @@ const COPLH = () => {
               </p>
             </div>
             <div className="relative ml-40">
-              <img
-                height={250}
-                width={250}
+              <Image
+                width={300}
+                height={300}
                 className="absolute right-0 bottom-10 mr-20 rounded-full"
                 src={"/assets/about/1570475856866.jpeg"}
                 alt={"about 1"}
@@ -242,8 +251,8 @@ const COPLH = () => {
           {/* LINE */}
           <div className="border-b-1 mt-5 flex-grow border-t-2 border-stone-800 " />
 
-          <div className="mt-10 flex flex-row">
-            <p className="flex-auto text-stone-400">ARCEL DESIGN</p>
+          <div className="mt-10 flex flex-col md:flex-row">
+            <p className="flex-auto text-stone-400">ARCELDESIGN, INC.</p>
             <div className="flex-auto">
               <ul>
                 <li>FACEBOOK</li>
@@ -267,109 +276,3 @@ const COPLH = () => {
 };
 
 export default COPLH;
-
-// import React from "react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore, { Autoplay, Navigation, EffectFade } from "swiper";
-// import { Keyboard, Pagination } from "swiper";
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/effect-fade";
-
-// interface Props {
-//   title: string;
-//   adjective: string;
-//   description: string;
-//   client: string;
-//   location: string;
-//   squareFeet: string;
-//   yearOfCompletion: string;
-// }
-
-// function ProjectDetail({
-//   title,
-//   adjective,
-//   description,
-//   client,
-//   location,
-//   squareFeet,
-//   yearOfCompletion,
-// }: Props) {
-//   return (
-//     <>
-//       {/* Title */}{" "}
-//       <div className="flex w-[500px] flex-col space-y-5 text-white">
-//         <h1 className="text-2xl">{title}</h1>
-//         <h2 className="text-xl">
-//           {adjective}... <span className="text-lg">{description}</span>
-//         </h2>
-//         <h2 className="text-xl">
-//           Client: <span className="text-lg">{client}</span>
-//         </h2>
-//         <h2 className="text-xl">
-//           Location: <span className="text-lg">{location}</span>
-//         </h2>
-//         <h2 className="text-xl">
-//           Square Footage: <span className="text-lg">{squareFeet}</span>
-//         </h2>
-//         <h2 className="text-xl">
-//           Year of Completion:{" "}
-//           <span className="text-lg">{yearOfCompletion}</span>
-//         </h2>
-//       </div>
-//     </>
-//   );
-// }
-
-// const COPLH = () => {
-//   const description =
-//     "Modern contemporary style, structure, and form combine harmoniously for this dental practice, creating an attractively ideal space, one minimal and therapeutic.";
-
-//   return (
-//     <>
-//       <Swiper
-//         slidesPerView={1}
-//         spaceBetween={30}
-//         keyboard={{
-//           enabled: true,
-//         }}
-//         pagination={{
-//           clickable: true,
-//         }}
-
-//         navigation={true}
-//         modules={[Keyboard, Pagination, Navigation]}
-//       >
-//         <SwiperSlide>
-
-//           <img src={"/assets/dental/dental1/IMG_6566.jpg"} alt={"dental 1"} />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={"/assets/dental/dental1/IMG_6567.jpg"} alt={"dental 1"} />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={"/assets/dental/dental1/IMG_6568.jpg"} alt={"dental 1"} />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={"/assets/dental/dental1/IMG_6569.jpg"} alt={"dental 1"} />
-//         </SwiperSlide>
-//         <SwiperSlide>
-//           <img src={"/assets/dental/dental1/IMG_6570.jpg"} alt={"dental 1"} />
-//         </SwiperSlide>
-//       </Swiper>
-
-//       <ProjectDetail
-//         title={"Comfort Dental"}
-//         client={"Julian Nguyen, DDS, MS"}
-//         description={description}
-//         adjective={"Modern Comfort"}
-//         location={"Pasadena, California"}
-//         squareFeet={"1500 sq ft | 4 Chairs"}
-//         yearOfCompletion={"2013"}
-//       />
-//     </>
-//   );
-// };
-
-// export default COPLH;
