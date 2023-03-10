a32bed2e35ecc9f2ef0198ae6aac3afb23b1c074
zachuri <punsalang.zachary@gmail.com>
2023-03-10 01:28:04 -0800

    layout: added gridlayout and gridlayoutmax -> for small and large items

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,3 +11,27 @@ export const AboutLayout: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   return <div className="max-md:mt-[59px]">{children}</div>;
 };
+
+// When gird layout has 6 or less
+export const GridLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div className="max-lg:mt-[130px]">
+      <div className="flex items-center justify-center lg:h-screen">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+// When grid has more than 6 use this layout
+export const GridLayoutMax: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div className="mt-[130px]">
+      <div className="flex items-center justify-center">{children}</div>
+    </div>
+  );
+};
