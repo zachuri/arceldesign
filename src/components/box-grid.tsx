afa608cee22db3ded288deb924c9f3e71616df27
zachuri <punsalang.zachary@gmail.com>
2023-03-10 07:01:33 -0800

    grid: was able to make a better dynamic grid that changes aspect ratio to square

--- src/components/box-grid.tsx
+++ src/components/box-grid.tsx
@@ -5,7 +5,7 @@ import { MainLayout } from "./layout";
 function Box({ src, alt }: { src: string; alt: string }) {
   return (
     <Link href={"/" + alt}>
-      <div className="b-5 relative p-[80px] text-lg sm:p-[120px] md:p-40">
+      <div className="relative aspect-square h-auto">
         <Image
           fill
           // allows object to fit as a cover instead of filled
@@ -18,7 +18,7 @@ function Box({ src, alt }: { src: string; alt: string }) {
           loading="eager"
         />
       </div>
-      <p className="mb-5 text-center uppercase text-white max-md:text-xs">
+      <p className="mt-5 aspect-auto text-center text-xs uppercase text-white md:text-base">
         {alt}
       </p>
     </Link>
@@ -26,18 +26,37 @@ function Box({ src, alt }: { src: string; alt: string }) {
 }
 
 interface Props {
+  total: number;
   images: { src: string; alt: string }[];
+  title: string;
 }
 
-const BoxGrid: React.FC<Props> = ({ images }) => {
+const BoxGrid: React.FC<Props> = ({ images, total, title }) => {
   return (
-    <div className="grid grid-cols-2 gap-5 md:grid-cols-2 lg:grid-cols-3">
-      {images.map((image) => {
-        return (
-          <Box key={image.src + image.alt} src={image.src} alt={image.alt} />
-        );
-      })}
-    </div>
+    <>
+      <div
+        className={
+          total < 6
+            ? "grid min-h-screen place-items-center"
+            : "mt-[60px] grid place-items-center py-10 md:mt-[82.5px]"
+        }
+      >
+        <div className="md:px-18 grid w-full grid-cols-2 gap-6 px-10 md:grid-cols-3 xl:grid-cols-4 xl:px-[350px]">
+          <h1 className="col-span-2 text-4xl uppercase text-white max-md:text-3xl md:col-span-3 xl:col-span-4">
+            {title}
+          </h1>
+          {images.map((image) => {
+            return (
+              <Box
+                key={image.src + image.alt}
+                src={image.src}
+                alt={image.alt}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,49 +3,11 @@ import React from "react";
 export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <div className="mt-[59px] md:mt-[83px]">{children}</div>;
+  return <div className="mt-[60px] md:mt-[82.5px]">{children}</div>;
 };
 
 export const AboutLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return <div className="max-md:mt-[59px]">{children}</div>;
-};
-
-// When gird layout has 6 or less
-export const GridLayout: React.FC<{
-  children: React.ReactNode;
-  total: number;
-  title: string;
-}> = ({ children, total, title }) => {
-  return (
-    <>
-      {total < 6 ? (
-        <div className="max-lg:mt-[90px]">
-          <div className="flex items-center justify-center lg:h-screen">
-            <div className="flex flex-col items-center max-sm:mx-7">
-              <div className="flex flex-col">
-                <h1 className="mb-5 text-2xl uppercase text-white md:mb-10 md:mt-10 md:text-4xl">
-                  {title}
-                </h1>
-                {children}
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="mt-[90px] md:mt-[130px]">
-          <div className="flex items-center justify-center max-sm:mx-7">
-            <div className="flex flex-col">
-              <h1 className="mb-5 text-2xl uppercase text-white md:mb-10 md:text-4xl">
-                {title}
-              </h1>
-
-              {children}
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
 };
