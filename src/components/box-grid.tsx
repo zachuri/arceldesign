33231ac0c957c8b3c874e21748e7600bc81d9d95
zachuri <punsalang.zachary@gmail.com>
2023-03-12 21:19:27 -0700

    grid: added padding for larger screensg

--- src/components/box-grid.tsx
+++ src/components/box-grid.tsx
@@ -9,7 +9,7 @@ const BoxGrid: React.FC<Props> = ({ children, title }) => {
   return (
     <>
       <div className={"mt-[60px] grid place-items-center py-10 md:mt-[82.5px]"}>
-        <div className="md:px-18 grid w-full grid-cols-2 gap-6 px-10 md:grid-cols-3 lg:px-44 xl:grid-cols-3 xl:px-64">
+        <div className="grid w-full grid-cols-2 gap-6 px-10 md:grid-cols-3 lg:px-48 xl:px-96">
           <h1 className="col-span-2 text-4xl uppercase text-white max-md:text-3xl md:col-span-3 xl:col-span-4">
             {title}
           </h1>
