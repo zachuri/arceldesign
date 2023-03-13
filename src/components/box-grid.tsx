cadea459d2e4e2e87129e5636a64781f7700f66f
zachuri <punsalang.zachary@gmail.com>
2023-03-12 21:14:53 -0700

    grid: fixed projects section for last row of grid to be centered

--- src/components/box-grid.tsx
+++ src/components/box-grid.tsx
@@ -2,14 +2,14 @@ import Box from "./box";
 
 interface Props {
   children: React.ReactNode;
-  title: string;
+  title?: string;
 }
 
 const BoxGrid: React.FC<Props> = ({ children, title }) => {
   return (
     <>
       <div className={"mt-[60px] grid place-items-center py-10 md:mt-[82.5px]"}>
-        <div className="md:px-18 grid w-full grid-cols-2 gap-6 px-10 md:grid-cols-3 lg:px-44 xl:grid-cols-4 xl:px-64">
+        <div className="md:px-18 grid w-full grid-cols-2 gap-6 px-10 md:grid-cols-3 lg:px-44 xl:grid-cols-3 xl:px-64">
           <h1 className="col-span-2 text-4xl uppercase text-white max-md:text-3xl md:col-span-3 xl:col-span-4">
             {title}
           </h1>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -23,18 +23,47 @@ const Projects = () => {
           address={""}
           type={"projects"}
         />
-        <Box
-          src={"/assets/medical/m1.jpg"}
-          alt={"medical"}
-          address={""}
-          type={"projects"}
-        />
-        <Box
-          src={"/assets/residential/r1.jpg"}
-          alt={"residential"}
-          address={""}
-          type={"projects"}
-        />
+
+        {/* Last two stay hidden when medium and up screen */}
+        <div className="md:hidden">
+          <Box
+            src={"/assets/medical/m1.jpg"}
+            alt={"medical"}
+            address={""}
+            type={"projects"}
+          />
+        </div>
+        <div className="md:hidden">
+          <Box
+            src={"/assets/residential/r1.jpg"}
+            alt={"residential"}
+            address={""}
+            type={"projects"}
+          />
+        </div>
+
+        {/* For medium and above screen -> center last row */}
+        {/* For smaller screens stay hidden */}
+        <div className="mt-10 max-md:hidden md:col-span-3 md:grid md:justify-items-stretch">
+          <div className="grid gap-6 md:grid-cols-6">
+            <div className="md:col-start-2 md:col-end-4">
+              <Box
+                src={"/assets/medical/m1.jpg"}
+                alt={"medical"}
+                address={""}
+                type={"projects"}
+              />
+            </div>
+            <div className="md:col-start-4 md:col-end-6">
+              <Box
+                src={"/assets/residential/r1.jpg"}
+                alt={"residential"}
+                address={""}
+                type={"projects"}
+              />
+            </div>
+          </div>
+        </div>
       </BoxGrid>
     </div>
   );
