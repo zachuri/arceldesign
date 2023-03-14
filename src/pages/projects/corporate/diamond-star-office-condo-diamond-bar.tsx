a46e4cf69ccd4a5bca34817742e2e1597d1c9833
zachuri <punsalang.zachary@gmail.com>
2023-03-14 08:47:32 -0700

    pages: added pages using project-input component

--- src/pages/projects/corporate/diamond-star-office-condo-diamond-bar.tsx
+++ src/pages/projects/corporate/diamond-star-office-condo-diamond-bar.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import AboutDesign from "../../../components/project-template/about-design";
-import Contact from "../../../components/project-template/contact/contact";
-import MastHead from "../../../components/project-template/mast-head";
+import ProjectInput from '../../../components/project-template/project-input';
 
 const images = [
   {
@@ -48,42 +45,7 @@ const info = {
 
 const DSOC = () => {
   return (
-    <>
-      <Head>
-        <title>ARCELDESIGN, INC. | {info.name}</title>
-        <meta name="description" content={info.name} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <MastHead
-          index={info.index}
-          designInfo={{
-            name: info.name,
-            type: info.type,
-            location: info.location,
-            completedYear: info.completedYear,
-          }}
-          image={images[0]?.src as string}
-        />
-        <AboutDesign
-          index={info.index}
-          designDesc={{
-            title: info.title,
-            desc: info.desc,
-          }}
-          designInfo={{
-            name: info.name,
-            type: info.type,
-            client: info.client,
-            size: info.size,
-            location: info.location,
-            completedYear: info.completedYear,
-          }}
-          images={images}
-        />
-        <Contact />
-      </main>
-    </>
+    <ProjectInput images={images} info={info}/>
   );
 };
 
