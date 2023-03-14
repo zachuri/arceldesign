d792980f448132aa06a54d29b29bd85b29ebe59b
zachuri <punsalang.zachary@gmail.com>
2023-03-14 08:42:41 -0700

    project-input: created a template for inputing data for each project page

---
+++ src/components/project-template/project-input.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Head from "next/head";
+import AboutDesign from "../../components/project-template/about-design";
+import Contact from "../../components/project-template/contact/contact";
+import MastHead from "../../components/project-template/mast-head";
+
+interface Props {
+  info: {
+    index: number;
+    name: string;
+    type: string;
+    client: string;
+    size: string;
+    location: string;
+    completedYear: string;
+    title: string;
+    desc: string;
+  };
+  images: { src: string; alt: string; desc: string }[];
+}
+
+const ProjectInput: React.FC<Props> = ({ info, images }) => {
+  return (
+    <>
+      <Head>
+        <title>ARCELDESIGN, INC. | {info.name}</title>
+        <meta name="description" content={info.name} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <MastHead
+          index={info.index}
+          designInfo={{
+            name: info.name,
+            type: info.type,
+            location: info.location,
+            completedYear: info.completedYear,
+          }}
+          image={images[0]?.src as string}
+        />
+        <AboutDesign
+          index={info.index}
+          designDesc={{
+            title: info.title,
+            desc: info.desc,
+          }}
+          designInfo={{
+            name: info.name,
+            type: info.type,
+            client: info.client,
+            size: info.size,
+            location: info.location,
+            completedYear: info.completedYear,
+          }}
+          images={images}
+        />
+        <Contact />
+      </main>
+    </>
+  );
+};
+
+export default ProjectInput;
